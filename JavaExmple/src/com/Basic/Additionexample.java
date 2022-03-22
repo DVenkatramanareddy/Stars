8248e6936f5f644fcb3eb0f342738cb91fb46bde
user <user@DESKTOP-8Q0AL96>
2022-03-22 08:09:20 +0530

    save java programs

---
+++ JavaExmple/src/com/Basic/Additionexample.java
@@ -0,0 +1,12 @@
+package com.Basic;
+
+public class Additionexample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=12,b=12,c;
+		c=a+b;
+		System.out.println(c);
+	}
+
+}
---
+++ JavaExmple/src/com/Basic/Helloworldexam.java
@@ -0,0 +1,11 @@
+package com.Basic;
+
+public class Helloworldexam {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world venkat");
+
+	}
+
+}
